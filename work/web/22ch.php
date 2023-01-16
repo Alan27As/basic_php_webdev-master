d3cb9876087f4edc1e8a5dc2898706cf7c16dcb8
Alan <newlife272331@gmail.com>
2023-01-16 10:41:03 +0900

    べにーとあれんの沼

--- work/web/22ch.php
+++ work/web/22ch.php
@@ -13,34 +13,41 @@
 <body>
   <?php
 
+$POSTS_FILE_NAME = "data.json";
+$data = file_get_contents($POSTS_FILE_NAME);
+// print_r($data);
+$posts = json_decode($data);
+print_r($posts);
 
-$data_file_name = "data.json";
-$data = file_get_contents($data_file_name);
-$posts = json_decode($data, true);
+// require_once("./Post.php");
+
+// $posts = readPostsFromJson($POSTS_FILE_NAME);
+// print_r($POSTS_FILE_NAME);
+
+// function readPostsFromJson()
+// {
+//   $posts[0] = new Post('100万人突破するまで生配信！');
+//   $posts[1] = new Post('頼む！行かないでくれ！');
+//   $posts[2] = new Post('登録即解除とかしたらどうなるん');
+//   $posts[3] = new Post('お前らは大嫌いだろうけど素直に凄いわ');
+
+//   return $posts;
+// }
+
+// $data_file_name = "data.json";
+// $data = file_get_contents($data_file_name);
+// $posts = json_decode($data, true);
 
 foreach($posts as $post) {
   echo "<div class ='card'>";
-  echo "<div class ='dttm'>" . $post["date"] . "</br></div>";
-  echo "<div class ='post'>" . $post["post"] . "</br></div>";
+  echo "<div class ='dttm'>" . $post->date . "</br></div>";
+  echo "<div class ='post'>" . $post->post . "</br></div>";
   echo "</div>";
   }
 
 
-// require_once('Post.php');
-
-  // $posts[0] = new Post('500万人突破するまで生配信！！！');
-  // $posts[1] = new Post('頼む！行かないでくれ！');
-  // $posts[2] = new Post('登録即解除とかしたらどうなるん');
-  // $posts[3] = new Post('お前らは大嫌いだろうけど素直に凄いわ');
-
-  // for ($i = 0; $i <= count($posts) - 1; $i++) {
-  //   echo "<div class ='card'>";
-  //   echo "<div class ='dttm'>" . $posts[$i]->dttm . "</br></div>";
-  //   echo "<div class ='post'>" . $posts[$i]->post . "</br></div>";
-  //   echo "</div>";
-  // }
   ?>
-  <h3>以上がポストです</h3>
+  <!-- <h3>以上がポストです</h3> -->
 </body>
 
 </html>
